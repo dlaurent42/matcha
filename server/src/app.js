bb8ae0b5af49cb78b35a63dcff0b81b531fabfc6
dlaurentn <dlaurent@student.42.fr>
2018-11-19 16:06:01 +0100

    [server] initalisation of app

--- server/src/app.js
+++ server/src/app.js
@@ -14,4 +14,10 @@ app.get('/status', (req, res) => {
   })
 })
 
+app.post('/register', (req, res) => {
+  res.send({
+    message: `Hello ${req.body.email}! Your user was registered!`,
+  })
+})
+
 app.listen(process.env.PORT || 8081)
