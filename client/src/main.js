56f68c029d7764bb8774af91901fdc72fdc822b6
HiDoc <fmadura@student.42.fr>
2018-12-03 15:56:35 +0100

    [package] add BootstrapVue

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,15 @@
+
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
 import App from './App'
 import router from './router'
+
+// Css import
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// NPM installed modules
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
