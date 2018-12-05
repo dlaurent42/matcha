e88c04a903578529c694972898cee3c5d9e67db2
dlaurentn <dlaurent@student.42.fr>
2018-12-05 14:50:29 +0100

    [install] remove docker container

--- install.sh
+++ install.sh
@@ -1,8 +1,3 @@
-# Install and run phpmyadmin
-docker run --name=mysql5 -e MYSQL_USER=root -e MYSQL_PASSWORD=password -d mysql/mysql-server:5.7 && \
-docker run --name phpmyadmin5 -d --link mysql5:db -p 8080:80 phpmyadmin/phpmyadmin
-docker start phpmyadmin5
-
 # Install client dependencies
 cd client
 npm install
