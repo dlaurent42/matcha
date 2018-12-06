9c7dbb85bbd6f104f5f18d74c7bf92a576ddd081
dlaurentn <dlaurent@student.42.fr>
2018-12-06 16:35:59 +0100

    [feature] auth

--- server/src/middlewares/auth.js
+++ server/src/middlewares/auth.js
@@ -4,17 +4,15 @@ const { isEmpty } = require('../utils')
 const authCredentials = (req, res, next) => {
   console.log(req.originalUrl)
   const bearerHeader = req.headers.authorization
-  if (typeof bearerHeader === 'undefined') {
-    return res.status(403).send({ err: 'No token provided.' })
-  }
+  if (typeof bearerHeader === 'undefined') return res.sendStatus(403)
   const jwt = new JsonWebToken()
   const bearerToken = bearerHeader.split(' ')[1]
   return jwt.refresh(bearerToken)
     .then((token) => {
-      if (isEmpty(token)) return res.status(403).send({ err: 'Wrong token provided.' })
+      if (isEmpty(token)) return res.sendStatus(403)
       return next()
     })
-    .catch(() => res.status(403).send({ err: 'An error occured.' }))
+    .catch(() => res.sendStatus(403))
 }
 
 module.exports = authCredentials
