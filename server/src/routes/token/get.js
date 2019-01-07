1ebb1bcbf1127bcf1068923eb8fceee8d3c82fce
dlaurentn <dlaurent@student.42.fr>
2019-01-07 13:52:38 +0100

    [server/fix] consistency between documentation and api

--- server/src/routes/token/get.js
+++ server/src/routes/token/get.js
@@ -1,12 +1,12 @@
 const router = require('express').Router()
-const User = require('../../models/User')
+const JsonWebToken = require('../../models/JsonWebToken')
 const { isEmpty } = require('../../utils')
 const { ERRORS } = require('../../config/constants').RESPONSES
 
 router.get('/', (req, res) => {
   if (isEmpty(req.body.token)) return res.status(400).json({ err: ERRORS.DATA_MISSING })
-  return new User().verifyIdentifiationToken(req.body.token)
-    .then(user => res.json({ user }))
+  return new JsonWebToken().check(req.body.token)
+    .then(data => res.json(data))
     .catch(err => res.json({ err: err.message }))
 })
 
