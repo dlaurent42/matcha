bfc6e14e0a555bbda74320c5ae68999e4ca9feba
dlaurentn <dlaurent@student.42.fr>
2019-02-12 13:15:20 +0100

    [API/feature] adapt token creation, duration = 365d

--- API/src/routes/token/post.js
+++ API/src/routes/token/post.js
@@ -1,12 +1,21 @@
 const router = require('express').Router()
-const JsonWebToken = require('../../models/JsonWebToken')
+const jwt = require('jsonwebtoken')
 const { isEmpty } = require('../../utils')
 const { ERRORS } = require('../../config/constants').RESPONSES
 const { JWT } = require('../../config/config')
 
+const create = data => (
+  new Promise((resolve, reject) => {
+    jwt.sign(data, JWT.SECRET, { expiresIn: '365d' }, (err, token) => {
+      if (!isEmpty(err)) return reject(err)
+      return resolve(token)
+    })
+  })
+)
+
 router.post('/', (req, res) => {
   if (isEmpty(req.body.data)) return res.status(400).json({ err: ERRORS.DATA_MISSING })
-  return new JsonWebToken().create(req.body.data)
+  return create(req.body.data)
     .then((token) => {
       if (isEmpty(token)) return res.json({ err: ERRORS.GENERAL })
       return res.json({ token, createdAt: Date.now(), expireAt: Date.now() + JWT.DURATION * 1000 })
